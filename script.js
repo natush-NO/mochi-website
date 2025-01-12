a889a9e951d17d78d2a8ae7044c3ae8378f1b2b4
natush <osmannatali22@gmail.com>
2025-01-12 19:18:34 +0100

    Creating a menu button

--- script.js
+++ script.js
@@ -4,19 +4,45 @@ console.clear();
 
 const contact = document.querySelector('[data-js="contact"]');
 const contactsSection = document.getElementById("contacts");
+const menuButton = document.getElementById("menu-button");
+const itemsInformation = document.getElementById("items-information");
 
-// Додаємо обробник події для кнопки
 contact.addEventListener("click", () => {
-  // Ховаємо вміст
-  // contactsSection.classList.add("hidden");
-  // Показуємо контакти
-  contactsSection.classList.remove("hidden-contact");
-  // contact.style.visibility = "hidden";
+  contactsSection.classList.remove("hidden");
 });
 
 contactsSection.addEventListener("click", () => {
-  // contact.classList.add("hidden");
-  contact.classList.remove("hidden-contact");
-  contactsSection.classList.add("hidden-contact");
-  // contact.style.visibility = "visible";
+  contact.classList.remove("hidden");
+  contactsSection.classList.add("hidden");
 });
+
+function checkScreenSize() {
+  if (window.innerWidth <= 950) {
+    menuButton.classList.remove("hidden");
+    itemsInformation.classList.add("hidden");
+    itemsInformation.style.position = "absolute";
+    itemsInformation.style.zIndex = "999";
+    itemsInformation.style.right = "50%";
+    itemsInformation.style.transform = "translateX(50%)";
+  } else {
+    menuButton.classList.add("hidden");
+    itemsInformation.classList.remove("hidden");
+    itemsInformation.style.position = "relative";
+    itemsInformation.style.right = "";
+    itemsInformation.style.transform = "";
+  }
+}
+
+menuButton.addEventListener("click", () => {
+  if (itemsInformation.classList.contains("hidden")) {
+    itemsInformation.classList.remove("hidden");
+    menuButton.textContent = "Close";
+  } else {
+    itemsInformation.classList.add("hidden");
+    menuButton.textContent = "Menu";
+  }
+});
+
+checkScreenSize();
+
+window.addEventListener("resize", checkScreenSize);
